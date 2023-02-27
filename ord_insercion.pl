bc6e157c98d86e6918f102ecf4448d10fc53a6d8
jcarpio <jose.carpio@gmail.com>
2023-02-27 08:56:29 +0100

    Create ord_insercion.pl

---
+++ ord_insercion.pl
@@ -0,0 +1,7 @@
+/*
+ord_insercion(+Lista, -R)
+  es cierto si R unifica con una lista que
+  contiene los elementos de lista ordenados
+  de menor a mayor. En lista podemos tener 
+  elementos repetidos.
+*/
