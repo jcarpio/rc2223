3c6ad78a3ce749821120dd61f4c6a62b9e92fdad
jcarpio <jose.carpio@gmail.com>
2023-03-07 10:29:41 +0100

    Update quicksort.pl

--- quicksort.pl
+++ quicksort.pl
@@ -9,23 +9,40 @@ divide(+Pivote, +Lista, -Menores, -Mayores)
 
 divide(_, [], [], []).
 
-divide(P, [Cab|Resto],  ,   ):-
+divide(P, [Cab|Resto], [Cab|Menores], Mayores):-
    Cab =< P,
    divide(P, Resto, Menores, Mayores).
-   
-   
-   
+
+divide(P, [Cab|Resto], Menores, [Cab|Mayores]):-
+   Cab > P,
+   divide(P, Resto, Menores, Mayores).
+
 /*
 
+quicksort(+Lista, -R)
+  es cierto si R unifica con una lista que contiene
+  los elementos de Lista ordenados de menor a mayore
+  por el método de QuickSort.  
+  
+*/
+
+quicksort([], []).
+quicksort([Cab|Resto], R):-
+   divide(Cab, Resto, Me, Ma),
+   quicksort(Me, RMe),
+   quicksort(Ma, RMa),
+   append(RMe, [Cab|RMa], R).
+      
+/*
 gen_lista(+N, +Rango, -Lista)
  es cierto si Lista unifica con una lista
  de enteros aleatorios entre 0 y Rango - 1. Lista
  tendrá longitud N.
-
 */
 
-gen_lista(0, []).
-
-crypto_data_hash(test, Hash, [algorithm(A)]).
+gen_lista(0, _, []).
+gen_lista(N, Rango, [Aleatorio|R]):- N > 0, N2 is N - 1,
+  gen_lista(N2, Rango, R),
+  random(0, Rango, Aleatorio).
 
 
