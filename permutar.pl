6cc69efd0861da6629266f04f1bdf54601390dbb
jcarpio <jose.carpio@gmail.com>
2023-03-14 13:55:49 +0100

    Create permutar.pl

---
+++ permutar.pl
@@ -0,0 +1,19 @@
+/*
+
+permutar(+Lista, R)
+  es cierto si obtengo en R por backtracking todas las
+  permutaciones.
+
+*/
+
+permutar([], []).
+permutar([Cab|Resto], R2):- permutar(Resto, R),
+                            insertar(Cab, R, R2).
+/*
+insertar(+E, +Lista, -R)
+  es cierto cuando R unifica con una lista que contiene
+  los elementos de Lista con E insertado en cualquier posición.
+*/
+
+insertar(E, Lista, [E|Lista]).
+insertar(E, [Cab|Resto], [Cab|R]):- insertar(E, Resto, R).
