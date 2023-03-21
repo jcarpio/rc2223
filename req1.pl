855c876a15da602024f7670c1f7dac333e30379d
jcarpio <jose.carpio@gmail.com>
2023-03-21 11:14:46 +0100

    Create req1.pl

---
+++ req1.pl
@@ -0,0 +1,14 @@
+slots_per_week(35).
+
+slots_per_day(7).
+
+class_subject_teacher_times('1a', deu, sjk1, 4).
+class_subject_teacher_times('1a', mat, mat1, 5).
+class_subject_teacher_times('1a', eng, anj1, 3).
+class_subject_teacher_times('1a', h, zgo1, 2).
+class_subject_teacher_times('1a', geo, geo1, 2).
+class_subject_teacher_times('1a', ch, kem1, 2).
+class_subject_teacher_times('1a', bio, bio1, 2).
+class_subject_teacher_times('1a', ph, fiz1, 2).
+class_subject_teacher_times('1a', f, rai1, 2).
+class_subject_teacher_times('1a', lat, atvz1, 3).
