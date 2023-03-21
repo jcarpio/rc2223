ac83676736a5f0d0d16cb970dcb603c6b98aa9a0
jcarpio <jose.carpio@gmail.com>
2023-03-21 13:21:47 +0100

    Update time1.pl

--- time1.pl
+++ time1.pl
@@ -1,4 +1,11 @@
-:- load_files('req1.pl').
+:- load_files('req2.pl').
+
+:- use_module(library(clpfd)).
+
+:- dynamic(room_alloc/4).
+
+:- discontiguous class_subject_teacher_times/4.
+:- discontiguous class_freeslot/2.
 
 classes(Classes) :-
        setof(C, S^N^T^class_subject_teacher_times(C,S,T,N), Classes).
@@ -6,6 +13,9 @@ classes(Classes) :-
 teachers(Teachers) :-
         setof(T, C^S^N^class_subject_teacher_times(C,S,T,N), Teachers).
 		
+rooms(Rooms) :-
+        findall(Room, room_alloc(Room,_C,_S,_Slot), Rooms0),
+        sort(Rooms0, Rooms).
 		
 requirements(Rs) :-
         Goal = class_subject_teacher_times(Class,Subject,Teacher,Number),
@@ -13,3 +23,17 @@ requirements(Rs) :-
         maplist(req_with_slots, Rs0, Rs).
 
 req_with_slots(R, R-Slots) :- R = req(_,_,_,N), length(Slots, N).
+
+
+pairs_slots(Ps, Vs) :-
+        pairs_values(Ps, Vs0),
+        append(Vs0, Vs).
+		
+sameroom_var(Reqs, r(Class,Subject,Lesson), Var) :-
+        memberchk(req(Class,Subject,_Teacher,_Num)-Slots, Reqs),
+        nth0(Lesson, Slots, Var).
+
+constrain_room(Reqs, Room) :-
+        findall(r(Class,Subj,Less), room_alloc(Room,Class,Subj,Less), RReqs),
+        maplist(sameroom_var(Reqs), RReqs, Roomvars),
+        all_different(Roomvars).
