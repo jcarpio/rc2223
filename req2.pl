ee225e602d5df60ecec5fb5a5bf4d1176f81865c
jcarpio <jose.carpio@gmail.com>
2023-03-21 13:37:42 +0100

    Create req2.pl

---
+++ req2.pl
@@ -0,0 +1,32 @@
+slots_per_week(35).
+
+slots_per_day(7).
+
+%class_subject_teacher_times('1a', ume, ume1, 2).
+%class_subject_teacher_times('1a', nej, nej1, 3).
+class_subject_teacher_times('1a', deu, sjk1, 4).
+class_subject_teacher_times('1a', mat, mat1, 5).
+class_subject_teacher_times('1a', eng, anj1, 3).
+class_subject_teacher_times('1a', h, zgo1, 2).
+class_subject_teacher_times('1a', geo, geo1, 2).
+class_subject_teacher_times('1a', ch, kem1, 2).
+class_subject_teacher_times('1a', bio, bio1, 2).
+class_subject_teacher_times('1a', ph, fiz1, 2).
+class_subject_teacher_times('1a', f, rai1, 2).
+class_subject_teacher_times('1a', lat, atvz1, 3).
+class_subject_teacher_times('1b', deu, sjk1, 4).
+class_subject_teacher_times('1b', mat, mat2, 5).
+class_subject_teacher_times('1b', eng, anj2, 3).
+class_subject_teacher_times('1b', nej, nej2, 3).
+class_subject_teacher_times('1b', h, zgo2, 2).
+class_subject_teacher_times('1b', geo, geo1, 2).
+class_subject_teacher_times('1b', ch, kem2, 2).
+class_subject_teacher_times('1b', bio, bio1, 2).
+class_subject_teacher_times('1b', ph, fiz2, 2).
+class_subject_teacher_times('1b', f, rai1, 2).
+class_subject_teacher_times('1b', ume, ume1, 2).
+class_subject_teacher_times('1b', lat, atvz2, 3).
+
+coupling('1a', deu, 2, 3).
+class_freeslot('1a', 0).
+class_freeslot('1a', 1).
