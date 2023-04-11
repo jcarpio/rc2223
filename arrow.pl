2194915f8c43cc5c27a970e75491c8f949dec3de
jcarpio <jose.carpio@gmail.com>
2023-04-11 13:59:03 +0200

    Update arrow.pl

--- arrow.pl
+++ arrow.pl
@@ -1,1 +1,11 @@
+
+
 cierto(X, Y, Z) :- ( X=1, Y=2 -> Z=3 ; X=2, Y=2, Z=2 ).
+
+
+% Equivalente a esto:
+% cierto(Z,Y,Z) :- X=1, Y=2 -> Z=3.
+% cierto(Z,Y,Z) :- X=2, Y=2 Z=2.
+
+% X=1, Y=2 -> Z=3 
+% Z=3 :- X=1, Y=2.
