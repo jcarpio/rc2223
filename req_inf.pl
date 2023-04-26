011015b172176000591f37dd55325de24e07444c
root <root@ip-172-31-1-20.eu-north-1.compute.internal>
2023-04-26 12:23:20 +0000

    dos ejemplos en servidor aws

---
+++ req_inf.pl
@@ -0,0 +1,21 @@
+slots_per_week(40).
+
+slots_per_day(8).
+
+class_subject_teacher_times('Inf1T1', 'Fundamentos de Computadores', fundcompInf1T1, 2).
+class_subject_teacher_times('Inf1T1', 'Fisica', fisicaInf1T1, 2).
+class_subject_teacher_times('Inf1T1', 'Fundamentos de Programacion', fundProgInf1T1, 2).
+class_subject_teacher_times('Inf1t1', 'Administración Y Economía de la Empresa', admEmpInf1T1, 2).
+class_subject_teacher_times('Inf1t1', 'Matematicas 1', matIinf1T1, 2).
+
+class_subject_teacher_times('Inf1T2', 'Fundamentos de Computadores', fundcompInf1T2, 2).
+class_subject_teacher_times('Inf1T2', 'Fisica', fisicaInf1T2, 2).
+class_subject_teacher_times('Inf1T2', 'Fundamentos de Programacion', fundProgInf1T2, 2).
+class_subject_teacher_times('Inf1t2', 'Administración Y Economía de la Empresa', admEmpInf1T2, 2).
+class_subject_teacher_times('Inf1t2', 'Matematicas 1', matIinf1T2, 2).
+
+class_subject_teacher_times('Inf1T3', 'Fundamentos de Computadores', fundcompInf1T3, 2).
+class_subject_teacher_times('Inf1T3', 'Fisica', fisicaInf1T3, 2).
+class_subject_teacher_times('Inf1T3', 'Fundamentos de Programacion', fundProgInf1T3, 2).
+class_subject_teacher_times('Inf1t3', 'Administración Y Economía de la Empresa', admEmpInf1T3, 2).
+class_subject_teacher_times('Inf1t3', 'Matematicas 1', matIinf1T3, 2).
